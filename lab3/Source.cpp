e5d8531c81f74a00cb9e320124f445f3b56e4d2f
lazaryan <31658948+lazaryan@users.noreply.github.com>
2017-11-21 23:43:30 +0300

    add lab3 version 1

---
+++ lab3/Source.cpp
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#define MAX_LENGTH 512
+
+#include <stdio.h> 
+#include <conio.h>
+#include <locale>
+
+void main(void) {
+	system("chcp 1251");
+	system("cls");
+
+	char	word[MAX_LENGTH] = "",
+			str[MAX_LENGTH]  = "";
+	printf("Введите строку:\n");
+	do {
+		scanf("%s", &word);
+
+		if (strlen(word) % 2 != 0) {
+			strcat(str, strcat(word," "));
+		}
+	} while (getchar() != '\n');
+
+	printf("\n%s", str);
+	_getch();
+}
